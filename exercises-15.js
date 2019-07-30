e2e52ae57d7831793c5e8818ce10f53864e37006
monogamus <monogamus@yahoo.com>
2019-07-30 22:34:42 +0700

    exe15

--- exercises-15.js
+++ exercises-15.js
@@ -1,4 +1,16 @@
-// Logic Challenge - Mengelompokkan Hewan
+/*
+Logic Challenge - Mengelompokkan Hewan
+
+Problem
+Diberikan sebuah function groupAnimals(arr) yang menerima satu parameter berupa array yang berisi string. Function akan me-return array multidimensi dimana array tersebut berisikan kategori/kelompok array yang dikumpulkan sesuai dengan abjad depannya. Abjad harus berurut dari a-z (jika ada). Untuk kasus ini, anggap saja semua text lowercase.
+
+Contoh jika animals adalah ["ayam", "kucing", "bebek", "bangau", "zebra"]
+
+maka akan menampilkan output: [ [ "ayam" ], [ "bebek", "bangau" ], [ "kucing ], [ "zebra" ] ]
+
+Urutan hewan dalam array setiap pengelompokan huruf tidak penting.
+*/
+
 function groupAnimals(animals) {
   var animalName = [ [] ];
   var a = 0;
